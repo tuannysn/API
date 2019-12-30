02a8bfc11ff931e67341223ede831658eacafcab
Tuanny <tuanny.nazareth@montreal.com.br>
2019-12-30 20:37:23 -0300

    subindo novos testes

--- RestAPI/src/main/java/br/com/RestAPI/rest/OlaMundo.java
+++ RestAPI/src/main/java/br/com/RestAPI/rest/OlaMundo.java
@@ -4,12 +4,16 @@ import static io.restassured.RestAssured.*;
 
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class OlaMundo {
 	
 	public static void main(String[] args) {
 		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
-		System.out.println(response.getBody().asString());
-		;
+		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+		System.out.println(response.statusCode()==200);
+		
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
 	}
 }
