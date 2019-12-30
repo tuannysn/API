de22a398674eb29e9e46b6db9df1b3125039e8dd
Tuanny <tuanny.nazareth@montreal.com.br>
2019-12-30 18:21:34 -0300

    Subindo projeto API Rest

---
+++ RestAPI/src/main/java/br/com/RestAPI/rest/OlaMundo.java
@@ -0,0 +1,15 @@
+package br.com.RestAPI.rest;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+public class OlaMundo {
+	
+	public static void main(String[] args) {
+		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+		System.out.println(response.getBody().asString());
+		;
+	}
+}
